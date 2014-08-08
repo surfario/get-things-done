5a3703ac40e8e072d1685c386e4db13bbb9701f4
Ari Olmos <ari.olmos@gmail.com>
2014-08-08 09:35:18 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GetThingsDone::Application.config.secret_key_base = '8ff4d6d640111b44b3d60a30aa2e18ae7bd1ca551a735024f3fc5467c11eda68ff2dbbb9d77ff89855b187f2062b7083731e70e556007480aa354d20b2d1e2ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetThingsDone::Application.config.session_store :cookie_store, key: '_get-things-done_session'

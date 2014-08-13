efab0dd88c06be368862561eddb65fc0897d104e
Ari Olmos <ari.olmos@gmail.com>
2014-08-13 16:58:52 -0700

    updated readme, updated task spec and models

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 This is Ari's self-destructing ToDo list app for Bloc.io.
-Created with Elad.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812155129) do
+ActiveRecord::Schema.define(version: 20140813220625) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "complete"
+    t.datetime "due_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "user_name"
